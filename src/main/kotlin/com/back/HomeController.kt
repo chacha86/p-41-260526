9e03ecf1717b339f0da5de4cf006573e0a918e24
chacha86 <taejincha0619@gmail.com>
2026-05-27 09:24:14 +0900

    deploy job add

--- src/main/kotlin/com/back/HomeController.kt
+++ src/main/kotlin/com/back/HomeController.kt
@@ -3,7 +3,6 @@ package com.back
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
-import software.amazon.awssdk.services.s3.S3Client
 
 @RestController
 class HomeController(
